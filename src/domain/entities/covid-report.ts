3ae6c8a0fbdc609a50de21f716ec1a672426f1cd
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-17 13:17:27 +0100

    add concrete implementation for CovidReport

--- src/domain/entities/covid-report.ts
+++ src/domain/entities/covid-report.ts
@@ -1,10 +1,20 @@
+export class CovidReportImpl implements CovidReport {
+  institutionId: string;
+  institutionName: string;
+  totalCases: number;
+  cases2021: number;
+
+  constructor({ institutionId, institutionName, totalCases, cases2021 }) {
+    this.institutionId = institutionId;
+    this.institutionName = institutionName;
+    this.totalCases = totalCases;
+    this.cases2021 = cases2021;
+  }
+}
+
 export default interface CovidReport {
-  date: Date;
-  state: string;
-  county: string;
-  city: string;
-  collegeId: string;
-  collegeName: string;
-  cases: number;
+  institutionId: string;
+  institutionName: string;
+  totalCases: number;
   cases2021: number;
 }

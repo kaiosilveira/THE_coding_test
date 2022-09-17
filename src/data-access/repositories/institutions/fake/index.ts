353b674063ee76a22bf84142d5fb641e46b7ed32
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-17 14:04:53 +0100

    add default return payload for institutions repo

--- src/data-access/repositories/institutions/fake/index.ts
+++ src/data-access/repositories/institutions/fake/index.ts
@@ -1,5 +1,16 @@
 import InstitutionsRepository from '..';
+import Institution from '../../../../domain/entities/institution';
 import institution from '../../../../domain/entities/institution';
+
+export const DEFAULT_PAYLOAD = [
+  {
+    name: 'Harvard University',
+    address: 'P.O. Box 114, 6922 Volutpat. Ave',
+    country: 'United States',
+    region: 'Middlesex, Cambridge, Massachusetts',
+    id: 'EA8BBED7-4106-94AF-48DD-A1414E386AFB',
+  } as Institution,
+];
 
 export default class FakeInstitutionsRepository implements InstitutionsRepository {
   list(): Promise<institution[]> {
